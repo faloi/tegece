6a058e84cedd18e55b6c31f37a19d0d99c2d3635
Ivan P. Oliveri <ipoliveri@gmail.com>
2012-09-20 01:22:20 -0300

    Ejemplo de Tanque en Sup Plana
    Es basicamente un tanque (TGCMesh) sobre un plano (TGCBox) que esta
    visto con una camara rotacional.

--- AlumnoEjemplos/RestrictedGL/EjemploTanque.cs
+++ AlumnoEjemplos/RestrictedGL/EjemploTanque.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using AlumnoEjemplos.RestrictedGL.GuiWrappers;
-using Microsoft.DirectX;
-using TgcViewer;
 using TgcViewer.Example;
+using TgcViewer;
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.TgcGeometry;
+using TgcViewer.Utils.Input;
+using Microsoft.DirectX.DirectInput;
 
 namespace AlumnoEjemplos.RestrictedGL
 {
     public class EjemploTanque : TgcExample
     {
-        private Terreno terreno;
+        private TgcBox surface;
+        private TgcMesh tank;
 
         public override string getCategory()
         {
@@ -28,15 +35,35 @@ namespace AlumnoEjemplos.RestrictedGL
         }
 
         public override void init() {
-            throw new NotImplementedException();
+
+            Microsoft.DirectX.Direct3D.Device d3DDevice = GuiController.Instance.D3dDevice;
+
+            //Carpeta de archivos Media del alumno
+            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
+
+
+            //Cargo la superficie y su textura
+            TgcTexture surfaceTexture = TgcTexture.createTexture(d3DDevice, alumnoMediaFolder + "RestrictedGL\\#TankExample\\Textures\\tierra.jpg");
+            surface = TgcBox.fromSize(new Vector3(0, 0, 0), new Vector3(500, 0, 500), surfaceTexture);
+
+            //Cargo el Tanque
+            TgcSceneLoader loader = new TgcSceneLoader();
+            TgcScene scene = loader.loadSceneFromFile(alumnoMediaFolder + "RestrictedGL\\#TankExample\\Scenes\\TanqueFuturistaOrugas-TgcScene.xml");
+            tank = scene.Meshes[0];
+
+            //Seteo la Camara
+            GuiController.Instance.RotCamera.setCamera(new Vector3(10f, 50f, 10f), 150f);
+
         }
 
         public override void close() {
-            throw new NotImplementedException();
+            surface.dispose();
+            tank.dispose();
         }
 
         public override void render(float elapsedTime) {
-            throw new NotImplementedException();
+            surface.render();
+            tank.render();
         }
     }
 }
