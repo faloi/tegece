02db1e70804a566f131375c2d9d507e2c99a1d8a
Federico <federico.aloi@gmail.com>
2012-09-20 00:19:45 -0300

    Agregado otro TgcExample para poder empezar con el tanque

---
+++ AlumnoEjemplos/RestrictedGL/EjemploTanque.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlumnoEjemplos.RestrictedGL.GuiWrappers;
+using Microsoft.DirectX;
+using TgcViewer;
+using TgcViewer.Example;
+
+namespace AlumnoEjemplos.RestrictedGL
+{
+    public class EjemploTanque : TgcExample
+    {
+        private Terreno terreno;
+
+        public override string getCategory()
+        {
+            return "AlumnoEjemplos";
+        }
+
+        public override string getName()
+        {
+            return "RestrictedGL.Tanque";
+        }
+
+        public override string getDescription()
+        {
+            return "Ejemplo del tanque sobre una superficie plana";
+        }
+
+        public override void init() {
+            throw new NotImplementedException();
+        }
+
+        public override void close() {
+            throw new NotImplementedException();
+        }
+
+        public override void render(float elapsedTime) {
+            throw new NotImplementedException();
+        }
+    }
+}
