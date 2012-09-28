3c5b820dd430c61f536b8ad94fb642ed466265bd
Federico <federico.aloi@gmail.com>
2012-09-28 03:11:10 -0300

    El Terrain ahora expone la matriz HeightmapData

--- AlumnoEjemplos/RestrictedGL/Terrain/Terrain.cs
+++ AlumnoEjemplos/RestrictedGL/Terrain/Terrain.cs
@@ -6,24 +6,27 @@ using TgcViewer.Utils.TgcSceneLoader;
 namespace AlumnoEjemplos.RestrictedGL.Terrain
 {
     public class Terrain : IRenderObject {
-        private const float SKYBOX_DEPTH = 9000f;
         private readonly List<IRenderObject> components;
+        private readonly AdaptativeHeightmap adaptativeHeightmap;
 
+        private const float SKYBOX_DEPTH = 9000f;
         private const int HEIGHTMAP_SIZE = 128;
         private const float INITIAL_SCALE_XZ = 20f;
         private const float INITIAL_SCALE_Y = 0.8f;
         private const float INITIAL_THRESHOLD = 0.075f;
 
-        private const int TREES_COUNT = 100;
+        private const int TREES_COUNT = 30;
+
+        public int[,] HeightmapData { get { return this.adaptativeHeightmap.HeightmapData; } }
 
         public Terrain() {
-            var adaptativeHeightmap = new AdaptativeHeightmap(INITIAL_SCALE_XZ, INITIAL_SCALE_Y, INITIAL_THRESHOLD);
+            this.adaptativeHeightmap = new AdaptativeHeightmap(INITIAL_SCALE_XZ, INITIAL_SCALE_Y, INITIAL_THRESHOLD);
 
             var treeFactory = new TreeFactory(INITIAL_SCALE_XZ, INITIAL_SCALE_Y);
             treeFactory.createTrees(TREES_COUNT, HEIGHTMAP_SIZE / 2, adaptativeHeightmap.HeightmapData);
 
             this.components = new List<IRenderObject> {
-               adaptativeHeightmap,
+               this.adaptativeHeightmap,
                treeFactory,
                new SkyBox(new Vector3(0, 0, 0), new Vector3(SKYBOX_DEPTH, SKYBOX_DEPTH, SKYBOX_DEPTH))
             };
