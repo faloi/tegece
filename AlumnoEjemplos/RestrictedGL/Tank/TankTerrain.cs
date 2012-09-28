e2df879546c4a9220ab352cfa5e88b029b9108ff
Federico <federico.aloi@gmail.com>
2012-09-28 03:10:36 -0300

    Eliminados los arboles del terreno del tanque

--- AlumnoEjemplos/RestrictedGL/Tank/TankTerrain.cs
+++ AlumnoEjemplos/RestrictedGL/Tank/TankTerrain.cs
@@ -12,30 +12,12 @@ namespace AlumnoEjemplos.RestrictedGL.Tank {
     class TankTerrain : IRenderObject {
         
         private TgcBox surface;
-        public List<TgcMesh> trees;
 
         const int SURFACE_SIZE = 500;
 
         private void createTrees(string alumnoMediaFolder) {
             var loader = new TgcSceneLoader();
             var scene = loader.loadSceneFromFile(alumnoMediaFolder + "RestrictedGL\\#TankExample\\Scenes\\TanqueFuturistaOrugas-TgcScene.xml");
-
-            var tree = scene.Meshes[1];
-            var treeSize = Convert.ToInt32(tree.BoundingBox.calculateSize().X);
-
-            this.trees = new List<TgcMesh>();
-            var randomizer = new Randomizer(50, SURFACE_SIZE / 2 - 2 * treeSize);
-
-            for (var i = 1; i < 10; i++)
-            {
-                var instance = tree.createMeshInstance(tree.Name + i);
-
-                var offsetX = randomizer.getNext();
-                var offsetZ = randomizer.getNext();
-                instance.move(offsetX, 0, offsetZ);
-
-                this.trees.Add(instance);
-            }
         }
 
         public void init(string alumnoMediaFolder){
@@ -52,9 +34,6 @@ namespace AlumnoEjemplos.RestrictedGL.Tank {
         }
 
         public bool isCollidingWith(TgcBoundingBox boundingBox) {
-            foreach (var obstaculo in this.trees) 
-                if (areColliding(boundingBox, obstaculo)) return true;
-
             return false;
         }
 
@@ -69,18 +48,10 @@ namespace AlumnoEjemplos.RestrictedGL.Tank {
             surface.render();
             if (showBoundingBox)
                 surface.BoundingBox.render();
-            
-            foreach (var tree in trees) {
-                tree.render();
-                if (showBoundingBox)
-                    tree.BoundingBox.render();
-            }
-
         }
     
         public void dispose() {
             surface.dispose();
-            trees.ForEach(t => t.dispose());
         }
 
         public bool AlphaBlendEnable { get; set; }
