9b00ca000d0ef70a87ed7b21a60ee791159d6e81
fdiazreal <f.diazreal@gmail.com>
2012-11-05 01:15:33 -0300

    Se agrega sublase TankPlayer y la TGCScene para el tanque enemigo

---
+++ AlumnoEjemplos/RestrictedGL/Tank/TankPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using AlumnoEjemplos.RestrictedGL.GuiWrappers;
+using AlumnoEjemplos.RestrictedGL.Interfaces;
+using AlumnoEjemplos.RestrictedGL.Utils;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using Microsoft.DirectX.DirectInput;
+using TgcViewer;
+using TgcViewer.Utils.Input;
+using TgcViewer.Utils.Sound;
+using TgcViewer.Utils.TgcGeometry;
+using TgcViewer.Utils.TgcSceneLoader;
+using AlumnoEjemplos.RestrictedGL.Terrain;
+
+namespace AlumnoEjemplos.RestrictedGL.Tank {
+    
+    class TankPlayer : Tank {
+
+        private TgcMesh turret;
+        private const float TURRET_ROTATION_FACTOR = 0.005f;
+        private float turretAngle;
+        private Vector3 offsetRotationCenter = new Vector3(0, 0, -5);
+
+        public TankPlayer (Vector3 initialPosition, Terrain.Terrain terrain, string scenePath) : base(initialPosition, terrain, scenePath) {
+            
+            var loader = new TgcSceneLoader { MeshFactory = new MeshShaderFactory() };
+            var turretScene = loader.loadSceneFromFile(Path.Turret);
+            this.turret = turretScene.Meshes[0];         
+            this.turretAngle = 0;
+
+            this.turret.AutoUpdateBoundingBox = this.turret.AutoTransformEnable = false;
+
+        }
+
+        public override void moveOrientedY(float movement) {
+           base.moveOrientedY(movement);
+           turret.moveOrientedY(movement);
+        }
+
+        public override void rotateY(float angle) {
+            turret.rotateY(angle);
+            base.rotateY(angle);
+        }
+
+        protected override void moveAndRotate() {
+            var d3DInput = Gui.I.D3dInput;
+            if (d3DInput.keyDown(Key.E))
+                this.turretAngle += TURRET_ROTATION_FACTOR;
+            if (d3DInput.keyDown(Key.Q))
+                this.turretAngle -= TURRET_ROTATION_FACTOR;
+            base.moveAndRotate();
+        }
+
+        public void rotateTurret() {
+           
+            this.turret.Transform = Matrix.Translation(offsetRotationCenter) * Matrix.RotationY(turretAngle) *
+                                   Matrix.Translation(offsetRotationCenter) * transformMatrix;
+
+        }
+
+        public override void render() {
+            base.render();
+            this.turret.BoundingBox.transform(transformMatrix);
+            this.turret.Transform = transformMatrix;
+            this.rotateTurret();
+            this.turret.render();
+        }
+
+        public override void dispose() {
+            this.turret.dispose();
+            base.dispose();
+        }
+    }
+}
